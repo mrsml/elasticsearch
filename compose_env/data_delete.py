338e3f615babee7ac855f178cc4ce2482012204a
mrsml <mrsmlklc@gmail.com>
2020-08-03 14:50:25 +0300

    Update data_delete.py

--- compose_env/data_delete.py
+++ compose_env/data_delete.py
@@ -8,7 +8,7 @@ from const import (
 es = Elasticsearch(host= ES_HOST, port= ES_PORT)
 es = Elasticsearch()
 
-
+#toplu silme işlemi
 def delete_data_by_bulk(_index, _doc_type, _id):
     action = [
         {
@@ -21,12 +21,12 @@ def delete_data_by_bulk(_index, _doc_type, _id):
     res = helpers.bulk(es, action)
     return res
   
-  
+#index ile silme işlemi
 def delete_index(_index):
     res = es.indices.delete(index=_index, ignore=[400, 404])
     return res
 
-
+#dizgi ile silme işlemi
 def delete_by_query(_index, query):
     res = es.delete_by_query(
         index=_index,
@@ -35,7 +35,7 @@ def delete_by_query(_index, query):
     )
     return res
 
-
+#tek veri silme işlemi
 def delete_one(_index, _id):
     res = es.delete(index=_index, id=_id)
     return res
